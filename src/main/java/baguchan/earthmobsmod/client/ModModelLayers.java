9db085dcd5503ea050c09ae1b381bbc641c0ac87
baguchan <shutux500@gmail.com>
2024-03-26 16:34:14 +0900

    1.20.4-16.3.1

--- src/main/java/baguchan/earthmobsmod/client/ModModelLayers.java
+++ src/main/java/baguchan/earthmobsmod/client/ModModelLayers.java
@@ -5,37 +5,43 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModModelLayers {
-    public static ModelLayerLocation CLUCK_SHROOM = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "cluck_shroom"), "cluck_shroom");
-    public static ModelLayerLocation FANCY_CHICKEN = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "fancy_chicken"), "fancy_chicken");
-    public static ModelLayerLocation HORNED_SHEEP = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "horned_sheep"), "horned_sheep");
-    public static ModelLayerLocation HORNED_SHEEP_FUR = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "horned_sheep_fur"), "horned_sheep_fur");
-    public static ModelLayerLocation HYPER_RABBIT = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "hyper_rabbit"), "hyper_rabbit");
-    public static ModelLayerLocation JUMBO_RABBIT = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "jumbo_rabbit"), "jumbo_rabbit");
+    public static ModelLayerLocation CLUCK_SHROOM = register("cluck_shroom");
+    public static ModelLayerLocation FANCY_CHICKEN = register("fancy_chicken");
+    public static ModelLayerLocation HORNED_SHEEP = register("horned_sheep");
+    public static ModelLayerLocation HORNED_SHEEP_FUR = register("horned_sheep_fur");
+    public static ModelLayerLocation HYPER_RABBIT = register("hyper_rabbit");
+    public static ModelLayerLocation JUMBO_RABBIT = register("jumbo_rabbit");
 
-    public static ModelLayerLocation TEACUP_PIG = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "teacup_pig"), "teacup_pig");
-    public static ModelLayerLocation MUDDY_PIG = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "muddy_pig"), "muddy_pig");
-    public static ModelLayerLocation JOLLY_LLAMA = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "jolly_llama"), "jolly_llama");
-
-
-    public static ModelLayerLocation BONE_SPIDER = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "bone_spider"), "bone_spider");
-    public static ModelLayerLocation STRAY_BONE_SPIDER = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "stray_bone_spider"), "stray_bone_spider");
-    public static ModelLayerLocation VILER_WITCH = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "viler_witch"), "viler_witch");
-    public static ModelLayerLocation BABY_GHAST = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "baby_ghast"), "baby_ghast");
-    public static ModelLayerLocation BABY_GHAST_CORE = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "baby_ghast_core"), "baby_ghast_core");
+    public static ModelLayerLocation TEACUP_PIG = register("teacup_pig");
+    public static ModelLayerLocation MUDDY_PIG = register("muddy_pig");
+    public static ModelLayerLocation JOLLY_LLAMA = register("jolly_llama");
 
 
-    public static ModelLayerLocation BOULDERING_ZOMBIE = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "bouldering_zombie"), "bouldering_zombie");
-	public static ModelLayerLocation LOBBER_ZOMBIE = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "lobber_zombie"), "lobber_zombie");
-
-	public static ModelLayerLocation BOULDERING_DROWNED = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "bouldering_drowned"), "bouldering_drowned");
-	public static ModelLayerLocation LOBBER_DROWNED = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "lobber_drowned"), "lobber_drowned");
-
-	public static ModelLayerLocation BOULDERING_DROWNED_OUTER = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "bouldering_drowned_outer"), "bouldering_drowned_outer");
-	public static ModelLayerLocation LOBBER_DROWNED_OUTER = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "lobber_drowned_outer"), "lobber_drowned_outer");
-    public static ModelLayerLocation MAGMA_COW = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "magma_cow"), "magma_cow");
-    public static ModelLayerLocation FURNACE_GOLEM = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "furnace_golem"), "furnace_golem");
-    public static ModelLayerLocation MOB_CHEST = new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, "mob_chest"), "mob_chest");
+    public static ModelLayerLocation BONE_SPIDER = register("bone_spider");
+    public static ModelLayerLocation STRAY_BONE_SPIDER = register("stray_bone_spider");
+    public static ModelLayerLocation VILER_WITCH = register("viler_witch");
+    public static ModelLayerLocation BABY_GHAST = register("baby_ghast");
+    public static ModelLayerLocation BABY_GHAST_CORE = register("baby_ghast_core");
 
 
+    public static ModelLayerLocation BOULDERING_ZOMBIE = register("bouldering_zombie");
+    public static ModelLayerLocation LOBBER_ZOMBIE = register("lobber_zombie");
 
+    public static ModelLayerLocation BOULDERING_DROWNED = register("bouldering_drowned");
+    public static ModelLayerLocation LOBBER_DROWNED = register("lobber_drowned");
+
+    public static ModelLayerLocation BOULDERING_DROWNED_OUTER = register("bouldering_drowned_outer");
+    public static ModelLayerLocation LOBBER_DROWNED_OUTER = register("lobber_drowned_outer");
+    public static ModelLayerLocation MAGMA_COW = register("magma_cow");
+    public static ModelLayerLocation FURNACE_GOLEM = register("furnace_golem");
+    public static ModelLayerLocation MOB_CHEST = register("mob_chest");
+
+
+    private static ModelLayerLocation register(String p_171294_) {
+        return register(p_171294_, "main");
+    }
+
+    private static ModelLayerLocation register(String p_171301_, String p_171302_) {
+        return new ModelLayerLocation(new ResourceLocation(EarthMobsMod.MODID, p_171301_), p_171302_);
+    }
 }
