2afd378e9121ef82892c335f3f9fbfb7c9c62b5b
baguchan <shutux500@gmail.com>
2025-03-27 01:27:30 +0900

    1.21.5-21.0.1

--- src/main/java/baguchan/earthmobsmod/client/model/CluckShroomModel.java
+++ src/main/java/baguchan/earthmobsmod/client/model/CluckShroomModel.java
@@ -14,6 +14,8 @@ public class CluckShroomModel<T extends CluckShroomRenderState> extends EntityMo
 	public static final String RED_THING = "red_thing";
 	public static final MeshTransformer BABY_TRANSFORMER = new BabyModelTransform(Set.of("head", "beak", "red_thing"));
 	private final ModelPart head;
+	private final ModelPart beak;
+	private final ModelPart red_thing;
 	private final ModelPart rightLeg;
 	private final ModelPart leftLeg;
 	private final ModelPart rightWing;
@@ -22,6 +24,8 @@ public class CluckShroomModel<T extends CluckShroomRenderState> extends EntityMo
 	public CluckShroomModel(ModelPart p_170490_) {
 		super(p_170490_);
 		this.head = p_170490_.getChild("head");
+		this.beak = p_170490_.getChild("beak");
+		this.red_thing = p_170490_.getChild("red_thing");
 		this.rightLeg = p_170490_.getChild("right_leg");
 		this.leftLeg = p_170490_.getChild("left_leg");
 		this.rightWing = p_170490_.getChild("right_wing");
@@ -57,6 +61,10 @@ public class CluckShroomModel<T extends CluckShroomRenderState> extends EntityMo
 		float f = (Mth.sin(p_364616_.flap) + 1.0F) * p_364616_.flapSpeed;
 		this.head.xRot = p_364616_.xRot * (float) (Math.PI / 180.0);
 		this.head.yRot = p_364616_.yRot * (float) (Math.PI / 180.0);
+		this.beak.xRot = this.head.xRot;
+		this.beak.yRot = this.head.yRot;
+		this.red_thing.xRot = this.head.xRot;
+		this.red_thing.yRot = this.head.yRot;
 		float f1 = p_364616_.walkAnimationSpeed;
 		float f2 = p_364616_.walkAnimationPos;
 		this.rightLeg.xRot = Mth.cos(f2 * 0.6662F) * 1.4F * f1;
