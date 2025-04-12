f79b77c5ccbada448ca4cef2c283c860b78a1755
baguchan <shutux500@gmail.com>
2025-04-12 21:32:30 +0900

    1.21.5-21.4.0 pot bugfix

--- src/main/java/baguchan/earthmobsmod/item/MobPotItem.java
+++ src/main/java/baguchan/earthmobsmod/item/MobPotItem.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.MobBucketItem;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
@@ -65,6 +66,10 @@ public class MobPotItem extends MobBucketItem {
             }
         }
         return InteractionResult.PASS;
+    }
+
+    public static ItemStack getEmptySuccessItem(ItemStack p_40700_, Player p_40701_) {
+        return !p_40701_.hasInfiniteMaterials() ? new ItemStack(Items.FLOWER_POT) : p_40700_;
     }
 
     @Override
