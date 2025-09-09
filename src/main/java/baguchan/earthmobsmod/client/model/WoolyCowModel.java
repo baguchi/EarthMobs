b5f773d37a0b43f7b9b1e219e9f3f04eb08543a6
baguchan <shutux500@gmail.com>
2025-09-09 17:05:08 +0900

    1.21.8-22.6.1

--- src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
+++ src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
@@ -14,7 +14,7 @@ import net.minecraft.util.Mth;
 import java.util.Set;
 
 public class WoolyCowModel<T extends WoolyCowRenderState> extends EntityModel<T> {
-    public static final MeshTransformer BABY_TRANSFORMER = new BabyModelTransform(Set.of("head"));
+    public static final MeshTransformer BABY_TRANSFORMER = new BabyModelTransform(false, 8.0F, 6.0F, Set.of("head"));
 
     private final ModelPart body;
     private final ModelPart wool_front;
@@ -45,33 +45,33 @@ public class WoolyCowModel<T extends WoolyCowRenderState> extends EntityModel<T>
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(18, 4).addBox(-6.0F, -10.0F, -7.0F, 12.0F, 18.0F, 10.0F, new CubeDeformation(0.0F))
-                .texOffs(52, 33).addBox(-2.0F, 1.75F, -8.0F, 4.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 5.0F, 2.0F, 1.5708F, 0.0F, 0.0F));
+        PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 8.0F, 6.0F, new CubeDeformation(0.0F))
+                .texOffs(22, 0).addBox(4.0F, -5.0F, -2.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(22, 0).addBox(-5.0F, -5.0F, -2.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(1, 33).addBox(-3.0F, 1.0F, -5.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 4.0F, -10.0F));
 
-        PartDefinition wool_front = body.addOrReplaceChild("wool_front", CubeListBuilder.create().texOffs(52, 3).addBox(0.0F, -4.0F, -3.0F, 0.0F, 8.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, -7.0F, 0.0F, 0.0F, -1.5708F));
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(18, 4).addBox(-6.0F, -10.0F, -6.0F, 12.0F, 18.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(52, 33).addBox(-2.0F, 1.75F, -7.0F, 4.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 6.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
 
-        PartDefinition wool_back = body.addOrReplaceChild("wool_back", CubeListBuilder.create().texOffs(52, 3).addBox(0.0F, -4.0F, -3.0F, 0.0F, 8.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 8.0F, -7.0F, 0.0F, 0.0F, -1.5708F));
+        PartDefinition wool_front = body.addOrReplaceChild("wool_front", CubeListBuilder.create().texOffs(52, 3).addBox(0.0F, -4.0F, -3.0F, 0.0F, 8.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -10.0F, -6.0F, 0.0F, 0.0F, -1.5708F));
 
-        PartDefinition wool_sideL = body.addOrReplaceChild("wool_sideL", CubeListBuilder.create(), PartPose.offset(6.0F, -1.0F, -7.0F));
+        PartDefinition wool_back = body.addOrReplaceChild("wool_back", CubeListBuilder.create().texOffs(52, 3).addBox(0.0F, -4.0F, -3.0F, 0.0F, 8.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 8.0F, -6.0F, 0.0F, 0.0F, -1.5708F));
+
+        PartDefinition wool_sideL = body.addOrReplaceChild("wool_sideL", CubeListBuilder.create(), PartPose.offset(6.0F, -1.0F, -6.0F));
 
         PartDefinition cube_r1 = wool_sideL.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(29, -11).addBox(0.0F, 0.0F, -5.5F, 0.0F, 3.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
 
-        PartDefinition wool_sideR = body.addOrReplaceChild("wool_sideR", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.0F, -1.0F, -7.0F, 0.0F, 0.0F, -3.1416F));
+        PartDefinition wool_sideR = body.addOrReplaceChild("wool_sideR", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.0F, -1.0F, -6.0F, 0.0F, 0.0F, -3.1416F));
 
         PartDefinition cube_r2 = wool_sideR.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(29, -11).addBox(0.0F, 0.0F, -5.5F, 0.0F, 3.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
 
-        PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -6.0F, 8.0F, 8.0F, 6.0F, new CubeDeformation(0.0F))
-                .texOffs(22, 0).addBox(4.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(22, 0).addBox(-5.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(1, 33).addBox(-3.0F, 1.0F, -7.0F, 6.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 4.0F, -8.0F));
+        PartDefinition left_hind_leg = partdefinition.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(4.0F, 12.0F, 6.0F));
 
-        PartDefinition left_hind_leg = partdefinition.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(4.0F, 12.0F, 7.0F));
+        PartDefinition right_hind_leg = partdefinition.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 12.0F, 6.0F));
 
-        PartDefinition right_hind_leg = partdefinition.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 12.0F, 7.0F));
+        PartDefinition left_front_leg = partdefinition.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -1.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(4.0F, 12.0F, -7.0F));
 
-        PartDefinition left_front_leg = partdefinition.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -1.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(4.0F, 12.0F, -6.0F));
-
-        PartDefinition right_front_leg = partdefinition.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -1.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 12.0F, -6.0F));
+        PartDefinition right_front_leg = partdefinition.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -1.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 12.0F, -7.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
