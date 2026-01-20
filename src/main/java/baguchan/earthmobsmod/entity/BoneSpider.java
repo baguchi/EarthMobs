a6ea058c077c6484c84a89bc46e8897965d05ce6
baguchan <shutux500@gmail.com>
2026-01-20 13:26:08 +0900

    1.21.11-25.4.0 fix bone spider mechanic cannot work

--- src/main/java/baguchan/earthmobsmod/entity/BoneSpider.java
+++ src/main/java/baguchan/earthmobsmod/entity/BoneSpider.java
@@ -39,7 +39,6 @@ import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
 import org.jspecify.annotations.Nullable;
 
-import java.util.Collection;
 import java.util.EnumSet;
 
 public class BoneSpider extends Spider implements RangedAttackMob {
@@ -218,8 +217,8 @@ public class BoneSpider extends Spider implements RangedAttackMob {
         double z = p_29912_.getZ() - this.getZ();
         double length = Math.sqrt(x * x + z * z);
         bone.shoot(x, y + (length * 0.275F), z, 0.75F, 2.0F);
-		Collection<MobEffectInstance> collection = this.getActiveEffects();
-		if (!collection.isEmpty()) {
+		PotionContents collection = this.getPotionContents();
+		if (collection.hasEffects()) {
             for (MobEffectInstance mobEffectInstance : this.getPotionContents().getAllEffects()) {
                 bone.addEffect(new MobEffectInstance(mobEffectInstance.getEffect(), mobEffectInstance.getDuration(), 0));
             }
