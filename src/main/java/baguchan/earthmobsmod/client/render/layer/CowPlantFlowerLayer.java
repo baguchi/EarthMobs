e5ae78d51c7f70f66784c34731e5f407a3da5b37
baguchi <shutux500@gmail.com>
2026-06-06 10:40:51 +0900

    fixed cow plant layer

--- src/main/java/baguchan/earthmobsmod/client/render/layer/CowPlantFlowerLayer.java
+++ src/main/java/baguchan/earthmobsmod/client/render/layer/CowPlantFlowerLayer.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.animal.cow.CowModel;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 
@@ -23,7 +24,7 @@ public class CowPlantFlowerLayer extends RenderLayer<MoobloomRenderState, CowMod
             p_117256_.mulPose(Axis.YP.rotationDegrees(-48.0F));
             p_117256_.scale(-1.0F, -1.0F, 1.0F);
             p_117256_.translate(-0.5F, -0.5F, -0.5F);
-            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state.outlineColor, state);
+            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state);
             p_117256_.popPose();
             p_117256_.pushPose();
             p_117256_.translate(0.2F, -0.35F, 0.5F);
@@ -32,7 +33,7 @@ public class CowPlantFlowerLayer extends RenderLayer<MoobloomRenderState, CowMod
             p_117256_.mulPose(Axis.YP.rotationDegrees(-48.0F));
             p_117256_.scale(-1.0F, -1.0F, 1.0F);
             p_117256_.translate(-0.5F, -0.5F, -0.5F);
-            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state.outlineColor, state);
+            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state);
             p_117256_.popPose();
             p_117256_.pushPose();
             (this.getParentModel()).getHead().translateAndRotate(p_117256_);
@@ -40,7 +41,7 @@ public class CowPlantFlowerLayer extends RenderLayer<MoobloomRenderState, CowMod
             p_117256_.mulPose(Axis.YP.rotationDegrees(-78.0F));
             p_117256_.scale(-1.0F, -1.0F, 1.0F);
             p_117256_.translate(-0.5F, -0.5F, -0.5F);
-            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state.outlineColor, state);
+            this.submitMushroomBlock(p_117256_, p_432964_, p_117258_, state);
             p_117256_.popPose();
         }
 
@@ -50,13 +51,14 @@ public class CowPlantFlowerLayer extends RenderLayer<MoobloomRenderState, CowMod
             PoseStack poseStack,
             SubmitNodeCollector submitNodeCollector,
             int lightCoords,
-            int overlayCoords,
             MoobloomRenderState state
     ) {
+        int overlayCoords = LivingEntityRenderer.getOverlayCoords(state, 0.0F);
+
         if (state.appearsGlowing() && state.isInvisible) {
-            state.plantBlock.submitOnlyOutline(poseStack, submitNodeCollector, lightCoords, overlayCoords, state.outlineColor);
+            state.plantBlock.submitOnlyOutline(poseStack, submitNodeCollector, lightCoords, state.outlineColor, overlayCoords);
         } else {
-            state.plantBlock.submit(poseStack, submitNodeCollector, lightCoords, overlayCoords, state.outlineColor);
+            state.plantBlock.submit(poseStack, submitNodeCollector, lightCoords, state.outlineColor, overlayCoords);
         }
     }
 }
